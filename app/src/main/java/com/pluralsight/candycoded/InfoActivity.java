12591513deebc2d18c6d5dbcc9e61c71ccb43902
victorbamikole <victorbamikole92@gmail.com>
2021-12-13 01:06:01 +0100

    phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -47,5 +47,11 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent dial = new Intent(Intent.ACTION_DIAL);
+        Uri uri = Uri.parse("tel:0123456789");
+        dial.setData(uri);
+        startActivity(dial);
+    }
 
 }
